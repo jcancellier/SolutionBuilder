47dab776148b7bed0b7931a027ea8f43800a978e
Joshua Cancellier <joshua.cancellier@apergy.com>
2020-01-22 13:20:11 -0800

    Add project files.

---
+++ SolutionBuilder/SolutionBuilderForm.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SolutionBuilderForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.chklistSolutions = New System.Windows.Forms.CheckedListBox()
+        Me.btnAddSolution = New System.Windows.Forms.Button()
+        Me.grpboxSolutions = New System.Windows.Forms.GroupBox()
+        Me.btnBuildSolutions = New System.Windows.Forms.Button()
+        Me.txtboxBuildOutput = New System.Windows.Forms.TextBox()
+        Me.grpboxSolutions.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'chklistSolutions
+        '
+        Me.chklistSolutions.Dock = System.Windows.Forms.DockStyle.Top
+        Me.chklistSolutions.FormattingEnabled = True
+        Me.chklistSolutions.Location = New System.Drawing.Point(3, 16)
+        Me.chklistSolutions.Name = "chklistSolutions"
+        Me.chklistSolutions.Size = New System.Drawing.Size(641, 94)
+        Me.chklistSolutions.TabIndex = 0
+        '
+        'btnAddSolution
+        '
+        Me.btnAddSolution.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.btnAddSolution.Location = New System.Drawing.Point(3, 116)
+        Me.btnAddSolution.Name = "btnAddSolution"
+        Me.btnAddSolution.Size = New System.Drawing.Size(641, 29)
+        Me.btnAddSolution.TabIndex = 1
+        Me.btnAddSolution.Text = "Add Solution"
+        Me.btnAddSolution.UseVisualStyleBackColor = True
+        '
+        'grpboxSolutions
+        '
+        Me.grpboxSolutions.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.grpboxSolutions.Controls.Add(Me.chklistSolutions)
+        Me.grpboxSolutions.Controls.Add(Me.btnAddSolution)
+        Me.grpboxSolutions.Location = New System.Drawing.Point(12, 12)
+        Me.grpboxSolutions.Name = "grpboxSolutions"
+        Me.grpboxSolutions.Size = New System.Drawing.Size(647, 148)
+        Me.grpboxSolutions.TabIndex = 2
+        Me.grpboxSolutions.TabStop = False
+        Me.grpboxSolutions.Text = "Solutions"
+        '
+        'btnBuildSolutions
+        '
+        Me.btnBuildSolutions.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.btnBuildSolutions.Location = New System.Drawing.Point(0, 319)
+        Me.btnBuildSolutions.Name = "btnBuildSolutions"
+        Me.btnBuildSolutions.Size = New System.Drawing.Size(671, 42)
+        Me.btnBuildSolutions.TabIndex = 3
+        Me.btnBuildSolutions.Text = "Build Solutions!"
+        Me.btnBuildSolutions.UseVisualStyleBackColor = True
+        '
+        'txtboxBuildOutput
+        '
+        Me.txtboxBuildOutput.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtboxBuildOutput.BackColor = System.Drawing.SystemColors.Control
+        Me.txtboxBuildOutput.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtboxBuildOutput.Enabled = False
+        Me.txtboxBuildOutput.Location = New System.Drawing.Point(12, 166)
+        Me.txtboxBuildOutput.Multiline = True
+        Me.txtboxBuildOutput.Name = "txtboxBuildOutput"
+        Me.txtboxBuildOutput.Size = New System.Drawing.Size(647, 147)
+        Me.txtboxBuildOutput.TabIndex = 4
+        '
+        'SolutionBuilderForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(671, 361)
+        Me.Controls.Add(Me.txtboxBuildOutput)
+        Me.Controls.Add(Me.btnBuildSolutions)
+        Me.Controls.Add(Me.grpboxSolutions)
+        Me.Name = "SolutionBuilderForm"
+        Me.Text = "Solution Builder"
+        Me.grpboxSolutions.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents chklistSolutions As CheckedListBox
+    Friend WithEvents btnAddSolution As Button
+    Friend WithEvents grpboxSolutions As GroupBox
+    Friend WithEvents btnBuildSolutions As Button
+    Friend WithEvents txtboxBuildOutput As TextBox
+End Class
---
+++ SolutionBuilder/SolutionBuilderForm.vb
@@ -0,0 +1,60 @@
+﻿Public Class SolutionBuilderForm
+    Private Sub btnAddSolution_Click(sender As Object, e As EventArgs) Handles btnAddSolution.Click
+        'Open File Dialog
+        Using openFileDialog As New OpenFileDialog()
+            openFileDialog.Filter = "Solution Files | *.sln"
+            If openFileDialog.ShowDialog() = DialogResult.OK Then
+                chklistSolutions.Items.Add(openFileDialog.FileName, True)
+            End If
+        End Using
+    End Sub
+
+    Private Sub btnBuildSolutions_Click(sender As Object, e As EventArgs) Handles btnBuildSolutions.Click
+        'Clear build output
+        txtboxBuildOutput.Clear()
+
+        Dim msbuildPath As String = """c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\amd64\MSbuild.exe"""
+        Dim solutionList As String = String.Join(" ", chklistSolutions.CheckedItems.Cast(Of String))
+        Dim buildCommand As String = $"/c ""{msbuildPath} -m:4 {solutionList}"" " '{msbuildPath} {solutionList} pause"
+
+        'Dim buildCommand As String = $"/c ""{msbuildPath} {solution}"" "
+
+        'TODO: use Process.Exited event to start next build
+
+        Dim processStartInfo As New ProcessStartInfo() With {
+            .FileName = "cmd.exe",
+            .Arguments = buildCommand,
+            .UseShellExecute = False,
+            .RedirectStandardOutput = True,
+            .RedirectStandardError = True,
+            .CreateNoWindow = True
+        }
+
+        Using process As Process = Process.Start(processStartInfo)
+            AddHandler process.OutputDataReceived, AddressOf HandleBuildOutput
+            AddHandler process.ErrorDataReceived, AddressOf HandleBuildError
+            process.BeginOutputReadLine()
+            process.BeginErrorReadLine()
+        End Using
+    End Sub
+
+    Private Sub HandleBuildOutput(sender As Object, e As DataReceivedEventArgs)
+        If e.Data = Nothing Then
+            Exit Sub
+        End If
+        txtboxBuildOutput.BeginInvoke(New MethodInvoker(
+            Sub()
+                txtboxBuildOutput.AppendText(vbCrLf & e.Data)
+            End Sub))
+    End Sub
+
+    Private Sub HandleBuildError(sender As Object, e As DataReceivedEventArgs)
+        If e.Data = Nothing Then
+            Exit Sub
+        End If
+        txtboxBuildOutput.BeginInvoke(New MethodInvoker(
+            Sub()
+                txtboxBuildOutput.AppendText(vbCrLf & e.Data)
+            End Sub))
+    End Sub
+End Class
