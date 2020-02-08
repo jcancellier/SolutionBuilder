7acefff5defe4be4d08505e10a8abfc61ca02ad2
Joshua Cancellier <joshua.cancellier@apergy.com>
2020-02-08 13:10:59 -0800

    Allow auto-detect of MSBuild in LocateMSBuildDialog

--- SolutionBuilder/Dialogs/LocateMSBuildDialog.Designer.vb
+++ SolutionBuilder/Dialogs/LocateMSBuildDialog.Designer.vb
@@ -22,67 +22,134 @@ Partial Class LocateMSBuildDialog
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.OK_Button = New System.Windows.Forms.Button()
         Me.Cancel_Button = New System.Windows.Forms.Button()
+        Me.btnAutoDetect = New System.Windows.Forms.Button()
+        Me.btnOpenFile = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.lblMSBuildInfo = New System.Windows.Forms.Label()
+        Me.lblResults = New System.Windows.Forms.Label()
+        Me.btnConfirm = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'TableLayoutPanel1
-        '
-        Me.TableLayoutPanel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.TableLayoutPanel1.ColumnCount = 2
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(277, 274)
-        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 1
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(146, 29)
-        Me.TableLayoutPanel1.TabIndex = 0
-        '
-        'OK_Button
-        '
-        Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.OK_Button.Location = New System.Drawing.Point(3, 3)
-        Me.OK_Button.Name = "OK_Button"
-        Me.OK_Button.Size = New System.Drawing.Size(67, 23)
-        Me.OK_Button.TabIndex = 0
-        Me.OK_Button.Text = "OK"
         '
         'Cancel_Button
         '
         Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.Cancel_Button.Location = New System.Drawing.Point(76, 3)
+        Me.Cancel_Button.Location = New System.Drawing.Point(358, 145)
         Me.Cancel_Button.Name = "Cancel_Button"
-        Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
+        Me.Cancel_Button.Size = New System.Drawing.Size(86, 23)
         Me.Cancel_Button.TabIndex = 1
         Me.Cancel_Button.Text = "Cancel"
         '
-        'FindMSBuildEXEDialog
+        'btnAutoDetect
         '
-        Me.AcceptButton = Me.OK_Button
+        Me.btnAutoDetect.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.btnAutoDetect.Location = New System.Drawing.Point(13, 145)
+        Me.btnAutoDetect.Name = "btnAutoDetect"
+        Me.btnAutoDetect.Size = New System.Drawing.Size(86, 23)
+        Me.btnAutoDetect.TabIndex = 2
+        Me.btnAutoDetect.Text = "Auto Detect"
+        '
+        'btnOpenFile
+        '
+        Me.btnOpenFile.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.btnOpenFile.Location = New System.Drawing.Point(105, 145)
+        Me.btnOpenFile.Name = "btnOpenFile"
+        Me.btnOpenFile.Size = New System.Drawing.Size(86, 23)
+        Me.btnOpenFile.TabIndex = 3
+        Me.btnOpenFile.Text = "Open File..."
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.TableLayoutPanel1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(456, 129)
+        Me.Panel1.TabIndex = 4
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.lblResults, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblMSBuildInfo, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(456, 129)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'lblMSBuildInfo
+        '
+        Me.lblMSBuildInfo.AutoSize = True
+        Me.lblMSBuildInfo.Location = New System.Drawing.Point(10, 10)
+        Me.lblMSBuildInfo.Margin = New System.Windows.Forms.Padding(10, 10, 3, 0)
+        Me.lblMSBuildInfo.Name = "lblMSBuildInfo"
+        Me.lblMSBuildInfo.Size = New System.Drawing.Size(418, 26)
+        Me.lblMSBuildInfo.TabIndex = 0
+        Me.lblMSBuildInfo.Text = "MSBuild is used to build Visual Studio Solutions. You may allow Solution Builder " &
+    "to auto-detect MSBuild, or you may manually locate the file. Select from the opt" &
+    "ions below."
+        '
+        'lblResults
+        '
+        Me.lblResults.AutoSize = True
+        Me.lblResults.Location = New System.Drawing.Point(10, 74)
+        Me.lblResults.Margin = New System.Windows.Forms.Padding(10, 10, 3, 0)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(0, 13)
+        Me.lblResults.TabIndex = 1
+        '
+        'btnConfirm
+        '
+        Me.btnConfirm.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.btnConfirm.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.btnConfirm.Enabled = False
+        Me.btnConfirm.Location = New System.Drawing.Point(266, 145)
+        Me.btnConfirm.Name = "btnConfirm"
+        Me.btnConfirm.Size = New System.Drawing.Size(86, 23)
+        Me.btnConfirm.TabIndex = 5
+        Me.btnConfirm.Text = "Confirm"
+        '
+        'LocateMSBuildDialog
+        '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.Cancel_Button
-        Me.ClientSize = New System.Drawing.Size(435, 315)
-        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.ClientSize = New System.Drawing.Size(456, 180)
+        Me.Controls.Add(Me.btnConfirm)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.btnOpenFile)
+        Me.Controls.Add(Me.btnAutoDetect)
+        Me.Controls.Add(Me.Cancel_Button)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.Name = "FindMSBuildEXEDialog"
+        Me.Name = "LocateMSBuildDialog"
         Me.ShowInTaskbar = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Locate MSBuild"
+        Me.Panel1.ResumeLayout(False)
         Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
-    Friend WithEvents OK_Button As System.Windows.Forms.Button
-    Friend WithEvents Cancel_Button As System.Windows.Forms.Button
 
+    Friend WithEvents Cancel_Button As Button
+    Friend WithEvents btnAutoDetect As Button
+    Friend WithEvents btnOpenFile As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents lblMSBuildInfo As Label
+    Friend WithEvents lblResults As Label
+    Friend WithEvents btnConfirm As Button
 End Class
--- SolutionBuilder/Dialogs/LocateMSBuildDialog.vb
+++ SolutionBuilder/Dialogs/LocateMSBuildDialog.vb
@@ -1,16 +1,35 @@
 ﻿Imports System.Windows.Forms
+Imports SolutionBuilder.Utilities
 
 Public Class LocateMSBuildDialog
     Public Property MSBuildFilePathResult As String = String.Empty
-
-    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
-        Me.DialogResult = System.Windows.Forms.DialogResult.OK
-        Me.Close()
-    End Sub
 
     Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
         Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.Close()
     End Sub
 
+    Private Sub btnAutoDetect_Click(sender As Object, e As EventArgs) Handles btnAutoDetect.Click
+        lblResults.ForeColor = DefaultForeColor
+        lblResults.ResetText()
+        lblResults.Text = $"Locating {Constants.MSBuildExe}..."
+        Cursor = Cursors.WaitCursor
+        MSBuildFilePathResult = LocateMSBuildService.Execute()
+        If String.IsNullOrEmpty(MSBuildFilePathResult) Then
+            lblResults.Text = $"Unable to auto-detect MSBuild.exe. Please try manually opening MSBuild.exe with the option below"
+            lblResults.ForeColor = Color.Red
+            'Focus Open File button
+            btnOpenFile.Select()
+        Else
+            lblResults.Text = "MSBuild found successfully: " & vbCrLf & MSBuildFilePathResult & vbCrLf & "Click Confirm to continue"
+            lblResults.ForeColor = DefaultForeColor
+            btnConfirm.Enabled = True
+            btnConfirm.Select()
+        End If
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub LocateMSBuildDialog_Load(sender As Object, e As EventArgs) Handles Me.Load
+        btnAutoDetect.Select()
+    End Sub
 End Class
